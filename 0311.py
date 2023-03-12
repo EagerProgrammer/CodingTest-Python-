875d08468a9dd24d987b0bec150449d6cae6199a
EagerProgrammer <yth960617@unist.ac.kr>
2023-03-12 23:51:18 +0900

    0311

---
+++ 0311.py
@@ -0,0 +1,18 @@
+#n이 만약 25로 주어진면 각 숫자에 해당된 알파벳중 하나씩 알파벳을 꺼내서 문자열을 만들고 리턴해준다.(대신 시작은 2부터 시작한다.)
+#예를 들어 n이 23이면 [a,b,c] 와 [d,e,f]의 두개의 배열에서 하나씩 골라서 만들수 있는 문자열을 리스트로 반환해준다.
+from itertools import product
+def solution(n):
+    list1 = n.split('')
+    list2 = [['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']]
+    list3 = []
+    list5 = []
+    for i in list1:
+        list3 += list2[i-2]
+    list3 = list(product(*list3))
+    for i in list3:
+        temp = ''
+        for j in i:
+            temp += j
+            if len(temp) == len(i):
+                list5.append(temp)
+    return list5
